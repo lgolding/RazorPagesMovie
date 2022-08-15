5bf4892e3a0d46ad76b5c47bc7cfdc206a5d7b9a
Larry Golding <larrygolding@comcast.net>
2022-08-15 14:40:20 -0700

    Support search on movie title in the query string.

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -16,11 +18,25 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string? MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
             if (_context.Movie != null)
             {
-                Movie = await _context.Movie.ToListAsync();
+                IQueryable<Movie> movies = _context.Movie;
+                if (!string.IsNullOrEmpty(SearchString))
+                {
+                    movies = movies.Where(m => m.Title.Contains(SearchString));
+                }
+
+                Movie = await movies.ToListAsync();
             }
         }
     }
